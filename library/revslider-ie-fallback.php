037a3d76bc4737b1a497205e9b52b3b85739e9f2
Eric Defore <eric.defore@gmail.com>
2018-03-15 13:46:37 -0400

    Finally working

--- library/revslider-ie-fallback.php
+++ library/revslider-ie-fallback.php
@@ -9,51 +9,54 @@
 // Exit if accessed directly
 if ( ! defined( 'ABSPATH' ) ) exit;
 
-add_filter( 'do_shortcode_tag', 'sonaria_revslider_ie_fallback', 10, 4 );
+// Replace Rev Slider shortcode with our Wrapper
+remove_shortcode( 'rev_slider', 'rev_slider_shortcode' );
+add_shortcode( 'rev_slider', 'sonaria_revslider_ie_fallback' );
 
 /**
- * Force the saved Fallback for Revslider to always show for all IE versions
+ * Override the Rev Slider shortcode with this wrapper to force the fallback on all IE versions, including Edge
  * 
- * @param		string $output            Shortcode Output
- * @param		string $tag               Shortcode Tag
- * @param		array  $args              Shortcode Args
- * @param		array  $regex_match_array Regex Match Array
- *                                               
+ * @param		array  $atts    Shortcode Atts
+ * @param		string $content Shortcode Content
+ *                        
  * @since		{{VERSION}}
  * @return		string Shortcode Output
  */
-function sonaria_revslider_ie_fallback( $output, $tag, $args, $regex_match_array ) {
+function sonaria_revslider_ie_fallback( $atts, $content ) {
 
-	if ( $tag == 'rev_slider' && 
-		sonaria_detect_ie() ) {
+	if ( sonaria_detect_ie() ) {
+		
+		$atts = shortcode_atts(
+			array( // a few default values
+				'alias' => '',
+				'settings' => '',
+				'order' => '',
+			),
+			$atts,
+			'rev_slider'
+		);
 
-		extract( shortcode_atts( array( 'alias' => '' ), $args, 'rev_slider' ) );
-		extract( shortcode_atts( array( 'settings' => '' ), $args, 'rev_slider' ) );
-		extract( shortcode_atts( array( 'order' => '' ), $args, 'rev_slider' ) );
+		if ( $atts['settings'] !== '' ) $atts['settings'] = json_decode( str_replace( array( '({', '})', "'" ), array( '[', ']', '"' ), $atts['settings'] ), true );
+		if ( $atts['order'] !== '' ) $atts['order'] = explode( ',', $atts['order'] );
 
-		if ( $settings !== '' ) $settings = json_decode( str_replace( array( '({', '})', "'" ), array( '[', ']', '"' ), $settings ), true );
-		if ( $order !== '' ) $order = explode( ',', $order );
-
-		$sliderAlias = ( $alias != '' ) ? $alias : RevSliderFunctions::getVal( $args, 0 );
-
-		$gal_ids = RevSliderFunctionsWP::check_for_shortcodes( $regex_match_array[5] ); // check for example on gallery shortcode and do stuff
-
-		if ( ! empty( $gal_ids ) ) { // add a gallery based slider
-			$slider = RevSliderOutput::putSlider( $sliderAlias, '', $gal_ids );
-		}
-		else {
-			$slider = RevSliderOutput::putSlider( $sliderAlias, '', array(), $settings, $order );
-		}
+		$sliderAlias = ( $atts['alias'] != '' ) ? $atts['alias'] : RevSliderFunctions::getVal( $atts, 0 );
+		
+		$slider = new RevSliderSlider();
+		$slider->initByAlias( $sliderAlias );
 
 		$show_alternate_image = $slider->getParam( "show_alternate_image", "" );
 		
 		// Fallback for the Fallback
-		if ( ! $show_alternate_image ) return $output;
+		if ( ! $show_alternate_image ) return rev_slider_shortcode( $atts, $content );
 		
 		return '<img class="tp-slider-alternative-image" src="' . $show_alternate_image . '" data-no-retina>';
 
 	}
-
-	return $output;
+	else {
+		
+		// Run the regular shortcode
+		return rev_slider_shortcode( $atts, $content );
+		
+	}
 
 }
